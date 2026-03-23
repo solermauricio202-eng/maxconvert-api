cebc1a4e36afb5836d466cfa5a0dd52131287279
solermauricio202-eng <solermauricio202@gmail.com>
2026-03-23 16:43:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -195,9 +195,6 @@ def convert_video():
         # Solo permitimos MP4 Estándar (480p) para que el servidor no explote
         if format == 'mp4':
             cmd.extend(['-c:v', 'libx264', '-preset', 'ultrafast', '-crf', '28', '-vf', 'scale=-1:480'])
-        else:
-            # Si el usuario intenta usar otro, lo forzamos a 480p por seguridad
-            cmd.extend(['-c:v', 'libx264', '-preset', 'ultrafast', '-vf', 'scale=-1:480'])
         elif format == 'mp4_hd':
             cmd.extend(['-c:v', 'libx264', '-crf', '35', '-preset', 'ultrafast', '-vf', 'scale=-1:720'])
         elif format == 'mp4_2k':
